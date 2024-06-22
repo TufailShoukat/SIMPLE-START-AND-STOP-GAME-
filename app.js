45aad848b971bd3d261597bc79247a9efc271a3b
Tufail Shoukat <tufailshoukat72@gmail.com>
2024-06-22 09:00:20 +0500

    app.js file created

---
+++ app.js
@@ -0,0 +1,81 @@
+
+function startcar(params) {
+    document.getElementById("img").src = "https://i.pinimg.com/originals/30/57/47/3057478343b774a2d117791160da8fc9.gif"
+    
+}
+function stopcar(params) {
+    document.getElementById("img").src =  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP5eMS3SAMc_iazKkDRlL858jJYu7fuZ6hSA83cK-hSUuXaRmPpoSpnIm-2NRwx9FhKms&usqp=CAU"
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getWeather() {
+//     const city = document.getElementById("citySelect").value;
+//     const weatherInfoDiv = document.getElementById("weatherInfo");
+
+//     const weatherData = {
+//         karachi: { temp: 32, condition: 'Sunny' },
+//         lahore: { temp: 35, condition: 'Hot' },
+//         islamabad: { temp: 28, condition: 'Cloudy' },
+//         peshawar: { temp: 34, condition: 'Partly Cloudy' },
+//         quetta: { temp: 25, condition: 'Windy' },
+//         faisalabad: { temp: 33, condition: 'Sunny' },
+//         multan: { temp: 36, condition: 'Hot' },
+//         rawalpindi: { temp: 27, condition: 'Rainy' },
+//         sialkot: { temp: 31, condition: 'Cloudy' },
+//         hyderabad: { temp: 30, condition: 'Sunny' }
+//     };
+
+//     const weather = weatherData[city];
+
+//     if (weather) {
+//         weatherInfoDiv.innerHTML = `<strong>Temperature:</strong> ${weather.temp}°C <br> <strong>Condition:</strong> ${weather.condition}`;
+//     } else {
+//         weatherInfoDiv.innerHTML = 'Weather information not available';
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
